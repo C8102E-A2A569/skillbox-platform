96bf409350c4fa953f2c9662d8e921068340faeb
C8102E-A2A569 <operatingsystem.c8102e@gmail.com>
2025-05-07 14:17:18 +0300

    second commit

--- catalog-service/src/main/java/com/skillbox/config/MongoConfig.java
+++ catalog-service/src/main/java/com/skillbox/config/MongoConfig.java
@@ -1,14 +1,10 @@
 package com.skillbox.config;
 
-import com.atomikos.icatch.jta.UserTransactionImp;
-import com.atomikos.icatch.jta.UserTransactionManager;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
